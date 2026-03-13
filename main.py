211eb54a9835e676cb84063e802183f274ff0c91
Isaac Kramer <isaacbkramer@gmail.com>
2026-03-13 08:51:03 -0400

    added csv import option to command line

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def startup():
     print('Follow the prompts or type "exit" at any time to terminate the program.\n')
     return register
 
-
 def shutdown(register:Register):
     register.connection.commit()
     register.connection.close()
@@ -20,7 +19,14 @@ if __name__ == "__main__":
     while True:
         command = input("(1)add (2)delete (3)modify (4)register (5)totals (6)exit: ")
         if command == '1':
-            register.addTransaction()
+            command = input("(1)add manual transaction (2) bulk import from csv: ")
+            if command == '1':
+                register.addTransaction()
+            elif command == '2': 
+                command = input("filename: ")
+                register.addTransactionsFromCsv(command)
+            else: 
+                continue
         elif command == '2':
             register.deleteTransaction()
         elif command == '3':
