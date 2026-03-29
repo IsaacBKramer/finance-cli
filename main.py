2a43a153d3cabe369e669707fad7eaf985114c64
Isaac Kramer <isaacbkramer@gmail.com>
2026-03-29 15:11:59 -0400

    read date from cli function to validate input

--- main.py
+++ main.py
@@ -15,6 +15,35 @@ def shutdown(register:Register):
     print('goodbye')
     sys.exit(0)
 
+def readDate():
+    while True:
+        dateStr = input("Date YYYYMMDD: ")
+        if len(dateStr) != 8:
+            print("incorrect date format")
+            continue
+        try:
+            year = int(dateStr[0:4])
+        except ValueError:
+            print(f"unexpected value for year {year}")
+            continue
+        try:
+            month = int(dateStr[4:6])
+        except ValueError:
+            print(f"unexpected value for month {month}")
+            continue    
+        if month < 1 or month > 12:
+            print(f"unexpected value for month {month}")
+            continue
+        try:
+            day = int(dateStr[6:])
+        except ValueError:
+            print(f"unexpected value for day {day}")
+            continue
+        if day < 1 or day > 31:
+            print(f"unexpected value for day {day}")
+            continue
+        return (year,month,day)
+
 if __name__ == "__main__":
     register = startup()
     while True:
@@ -25,15 +54,12 @@ if __name__ == "__main__":
                 if command =='add':
                     command = input("manual csv: ").lower()
                     if command == 'manual':
-                        date = input("Date YYYYMMDD: ")
-                        year = int(date[0:4])
-                        month = int(date[4:6])
-                        day = int(date[6:])
+                        date = readDate()
                         value = float(input("Value: "))
                         account = str(input("Account Name: "))
                         category = str(input("Category: "))
                         tag = str(input("Tag: "))
-                        register.addTransaction(year,month,day,value,account,category,tag)
+                        register.addTransaction(date[0],date[1],date[2],value,account,category,tag)
                     elif command == 'csv': 
                         command = input("filename: ")
                         file = Path(command)
