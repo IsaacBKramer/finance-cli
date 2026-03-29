1d2099e59e74c81018a900c060c9980c2d900e8e
Isaac Kramer <isaacbkramer@gmail.com>
2026-03-29 14:36:05 -0400

    adjusted cli inputs to not exit back to main menu

--- main.py
+++ main.py
@@ -20,43 +20,55 @@ if __name__ == "__main__":
     while True:
         command = input("transactions accounts totals exit: ")
         if command == 'transactions':
-            command = input("add delete modify view exit: ")
-            if command =='add':
-                command = input("manual csv: ")
-                if command == 'manual':
-                    date = input("Date YYYYMMDD: ")
-                    year = int(date[0:4])
-                    month = int(date[4:6])
-                    day = int(date[6:])
-                    value = float(input("Value: "))
-                    account = str(input("Account Name: "))
-                    category = str(input("Category: "))
-                    tag = str(input("Tag: "))
-                    register.addTransaction(year,month,day,value,account,category,tag)
-                elif command == 'csv': 
-                    command = input("filename: ")
-                    file = Path(command)
-                    if file.is_file():
-                        register.addTransactionsFromCsv(command)
-                    else:
-                        print(f'file {command} does not exist')
-                else: 
+            while True:
+                command = input("add delete modify view exit: ")
+                if command =='add':
+                    command = input("manual csv: ")
+                    if command == 'manual':
+                        date = input("Date YYYYMMDD: ")
+                        year = int(date[0:4])
+                        month = int(date[4:6])
+                        day = int(date[6:])
+                        value = float(input("Value: "))
+                        account = str(input("Account Name: "))
+                        category = str(input("Category: "))
+                        tag = str(input("Tag: "))
+                        register.addTransaction(year,month,day,value,account,category,tag)
+                    elif command == 'csv': 
+                        command = input("filename: ")
+                        file = Path(command)
+                        if file.is_file():
+                            register.addTransactionsFromCsv(command)
+                        else:
+                            print(f'file {command} does not exist')
+                    else: 
+                        continue
+                elif command == 'delete':
+                    id = int(input("id: "))
+                    register.deleteTransaction(id)
+                elif command == 'modify':
+                    id = int(input("id: "))
+                    register.modifyTransaction(id)
+                elif command == 'view':
+                    register.viewTransactions()
+                elif command == 'exit':
+                    break
+                else:
+                    print('invalid input')
                     continue
-            elif command == 'delete':
-                id = int(input("id: "))
-                register.deleteTransaction(id)
-            elif command == 'modify':
-                id = int(input("id: "))
-                register.modifyTransaction(id)
-            elif command == 'view':
-                register.viewTransactions()
         elif command == 'accounts':
-            command = input("add view: ")
-            if command == 'add':
-                accountName = input("account name: ")
-                register.addAccount(accountName)
-            elif command == 'view':
-                register.viewAccounts()
+            while True:
+                command = input("add view exit: ")
+                if command == 'add':
+                    accountName = input("account name: ")
+                    register.addAccount(accountName)
+                elif command == 'view':
+                    register.viewAccounts()
+                elif command == 'exit':
+                    break
+                else:
+                    print('invalid input')
+                    continue
         elif command == 'totals':
             command = input("year account: ")
             if command == 'year':
