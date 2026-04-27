3c433f9bdbba04e502cd250b48abd3e0716d6e56
Isaac Kramer <isaacbkramer@gmail.com>
2026-04-27 00:17:02 -0400

    updated investments tests

--- tests/test_finance.py
+++ tests/test_finance.py
@@ -19,6 +19,7 @@ def test_tables(database):
 
 def test_accounts(database):
     assert acc.addAccount(database, 'SAVINGS') == True
+    assert acc.addAccount(database, 'BROKERAGE') == True
 
 def test_valid_transaction(database):
     assert reg.addTransaction(database, 2026, 4, 25, 100.00, 'SAVINGS', 'INCOME', 'NONE') == True
@@ -30,9 +31,9 @@ def test_valid_transaction(database):
 def test_invalid_transaction(database):
     assert reg.addTransaction(database, 2026, 4, 25, 100.00, 'CHECKING', 'INCOME', 'NONE') == False
 
-def test_investments(database):
-    assert inv.addInvestment(database, 2026, 4, 25, 'VTSAX', 500, 1.0) == True
-    assert inv.addInvestment(database, 2026, 4, 25, 'FXAIX', 1000, 2.0) == True
+def test_valid_investments(database):
+    assert inv.addInvestment(database, 2026, 4, 25, 'VTSAX', 500, 1.0, 'BROKERAGE') == True
+    assert inv.addInvestment(database, 2026, 4, 25, 'FXAIX', 1000, 2.0, 'BROKERAGE') == True
 
     totals = inv.getInvestments(database)
 
@@ -43,3 +44,10 @@ def test_investments(database):
     series = totals.loc[totals['Ticker'] == 'FXAIX', 'Shares']
     assert len(series) == 1
     assert series.iloc[0] == 2.0
+
+def test_invalid_investment(database):
+    assert inv.addInvestment(database, 2026, 4, 25, 'FXAIX', 1000, 2.0, 'IRA') == False
+
+def test_get_tickers(database):
+    tickers = inv.getTickers(database)
+    assert len(tickers) == 2
